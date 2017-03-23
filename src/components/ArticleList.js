fe6d16d9b0d931e5e75951b90e8c133e58a2a4a0
William Pickering <willpicky@gmail.com>
2017-03-23 02:13:56 +0000

    Article List component file created & setup

---
+++ src/components/ArticleList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ArticleList = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Article List</h1>
+      </div>
+    );
+  }
+});
+
+export default ArticleList;
