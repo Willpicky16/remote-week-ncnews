0180dc2f745dc20ffd2785305a5e6dc1ff715ebf
William Pickering <willpicky@gmail.com>
2017-03-29 09:28:45 +0100

    Index reducer updated

--- src/reducers/index.reducer.js
+++ src/reducers/index.reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import articles from './articles.reducer.js';
+import comments from './comments.reducer.js';
 
 export default combineReducers ({
-  articles
+  articles,
+  comments
 });
