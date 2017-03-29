fc74cb3c42092a54aa22020caa49877ca09df80e
William Pickering <willpicky@gmail.com>
2017-03-29 09:26:31 +0100

    Comments action added

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -35,3 +35,38 @@ export function fetchArticlesError (err) {
     data: err
   };
 }
+
+export function fetchAllComments (article_id) {
+  return function (dispatch) {
+    dispatch(fetchCommentsRequest());
+    console.log(`${ROOT}/articles/${article_id}/comments`);
+    axios
+      .get(`${ROOT}/articles/${article_id}/comments`)
+      .then((res) => {
+        dispatch(fetchCommentsSuccess(res.data.comments));
+      })
+      .catch((err) => {
+        dispatch(fetchCommentsError(err));
+      });
+  };
+}
+
+export function fetchCommentsRequest () {
+  return {
+    type: types.FETCH_COMMENTS_REQUEST
+  };
+}
+
+export function fetchCommentsSuccess (comments) {
+  return {
+    type: types.FETCH_COMMENTS_SUCCESS,
+    data: comments
+  };
+}
+
+export function fetchCommentsError (err) {
+  return {
+    type: types.FETCH_COMMENTS_ERROR,
+    data: err
+  };
+}
