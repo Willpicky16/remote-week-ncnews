6763776822e14532d0f7c01ec02fa8afaf446802
William Pickering <willpicky@gmail.com>
2017-04-04 09:26:53 +0100

    users reducer added to combined reducer

--- src/reducers/index.reducer.js
+++ src/reducers/index.reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import articles from './articles.reducer.js';
 import comments from './comments.reducer.js';
+import users from './users.reducer.js';
 
 export default combineReducers ({
   articles,
-  comments
+  comments,
+  users
 });
